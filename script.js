dbf631a9ab2ade6a27ae9c50c3e45c3b7d5c192c
Ankita Sood <ankisood@amazon.com>
2024-07-25 18:48:19 +0530

    Added date and time to the list item and small css changes

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ addItem.addEventListener("click", () => {
     const listItem = document.createElement("div");
     listItem.innerHTML = `<p class="textInput strikeOf">${
       inputData[0].toUpperCase() + inputData.slice(1)
-    }</p> <p><button class="done">✅</button><button class="cancel">❌</button></p>`;
+    }</p> <p> ${printDateAndTime()}<button class="done">✅</button><button class="cancel">❌</button></p>`;
     listItem.classList.add("item");
     item.prepend(listItem);
 
@@ -18,6 +18,7 @@ addItem.addEventListener("click", () => {
       const textInput = listItem.querySelector(".textInput");
       textInput.classList.toggle("strikeOn");
       textInput.classList.toggle("strikeOf");
+      listItem.classList.toggle("fade");
     });
 
     // when task is deleted from to do list
@@ -33,12 +34,16 @@ addItem.addEventListener("click", () => {
   }
 });
 
+const printDateAndTime = function() {
+ return new Intl.DateTimeFormat("en-US").format(new Date()); 
+}
+
 // Future features ->
 /*
  1. add a icon ❌  to right of added element which can allow us to delete the item list. - done
  2. add a icon ✅ to the right of added element to strike of the task. - done
  3. add search capability to the added list items - (advance but good to know).
- 4. add creation date and time to the to do list item ( to display when this item was added).
+ 4. add creation date and time to the to do list item ( to display when this item was added). - done
  5. when button is clicked without any input element, throw error message input field is empty
  6. To add percentage and progress bar - Initially set to 0% and no progress 
  7. Add some additional peices of data to do list : todays's date, no. of tasks, a simple quote Get things done, one item at a time. 
